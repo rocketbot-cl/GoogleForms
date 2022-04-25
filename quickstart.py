f1832d9dec76bd6f3151631e3f538879d2e3b0bb
Auca Maillot <auca.maillot@rocketbot.com>
2022-04-25 17:49:04 -0300

    [new] added sample file to create a simple google form quickstart.py

---
+++ quickstart.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+
+# Google libraries
+from googleapiclient import discovery
+from httplib2 import Http
+from google_auth_oauthlib.flow import InstalledAppFlow
+# docs
+# https://github.com/googleapis/google-api-python-client/blob/main/docs/README.md
+
+
+SCOPES = "https://www.googleapis.com/auth/forms.body"
+DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
+
+creds = None
+
+CLIENT_SECRETS_FILE = 'client_secrets.json'
+
+if not creds or creds.invalid:
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    creds = flow.run_console()
+
+form_service = discovery.build('forms', 'v1', credentials=creds)
+
+# Request body for creating a form
+NEW_FORM = {
+    "info": {
+        "title": "Quickstart form",
+    }
+}
+
+# Request body to add a multiple-choice question
+NEW_QUESTION = {
+    "requests": [{
+        "createItem": {
+            "item": {
+                "title": "In what year did the United States land a mission on the moon?",
+                "questionItem": {
+                    "question": {
+                        "required": True,
+                        "choiceQuestion": {
+                            "type": "RADIO",
+                            "options": [
+                                {"value": "1965"},
+                                {"value": "1967"},
+                                {"value": "1969"},
+                                {"value": "1971"}
+                            ],
+                            "shuffle": True
+                        }
+                    }
+                },
+            },
+            "location": {
+                "index": 0
+            }
+        }
+    }]
+}
+
+# Creates the initial form
+result = form_service.forms().create(body=NEW_FORM).execute()
+
+# Adds the question to the form
+question_setting = form_service.forms().batchUpdate(formId=result["formId"], body=NEW_QUESTION).execute()
+
+# Prints the result to show the question has been added
+get_result = form_service.forms().get(formId=result["formId"]).execute()
+print(get_result)
