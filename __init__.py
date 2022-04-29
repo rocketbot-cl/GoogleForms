4db39989eb3c348e14d2b51327ea629dc52047f2
Auca Maillot <aucacoyan@gmail.com>
2022-04-29 13:03:55 -0300

    [new] logic for credentials working

--- __init__.py
+++ __init__.py
@@ -26,12 +26,13 @@ Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
 
 import os
 import json
+
 # import sys # it's already in rocketbot main
 
 # Add the libs folder to the system path
-base_path = tmp_global_obj["basepath"] #type:ignore
-google_directory_path = os.path.join(base_path, "modules", "GoogleAdmin")
-gd_libs_path = os.path.join(google_directory_path, "libs") #type:ignore
+base_path = tmp_global_obj["basepath"]  # type:ignore
+google_directory_path = os.path.join(base_path, "modules", "GoogleForms")
+gd_libs_path = os.path.join(google_directory_path, "libs")  # type:ignore
 
 if gd_libs_path not in sys.path:
     sys.path.append(gd_libs_path)
@@ -42,10 +43,10 @@ The syntax is {"session name": {data}}
 """
 SESSION_DEFAULT = "default"
 try:
-    if not mod_google_directory:                #type:ignore
-        mod_google_directory = {SESSION_DEFAULT:{}}
+    if not mod_google_directory:  # type:ignore
+        mod_google_directory = {SESSION_DEFAULT: {}}
 except NameError:
-    mod_google_directory = {SESSION_DEFAULT:{}}
+    mod_google_directory = {SESSION_DEFAULT: {}}
 
 
 class GoogleDirectory:
@@ -55,69 +56,184 @@ class GoogleDirectory:
         e: _description_
     """
 
-    def __init__(self, credential_path, token_path='token.json') -> None:
+    def __init__(self, credential_path) -> None:
         self.SCOPES = "https://www.googleapis.com/auth/forms.body"
         self.DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
         self.credential_path = credential_path
-        self.token_path = token_path
         self.form_service = None
-    
+
     def config_credentials(self):
         """Configure the credentials."""
         from google_auth_oauthlib.flow import InstalledAppFlow
-        from google.oauth2.credentials import Credentials
-        from google.auth.transport.requests import Request
-        from googleapiclient.discovery import build
+        from googleapiclient import discovery
 
         self.creds = None
         # The file token.json stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists(self.token_path):
-            print("Loading credentials from token.json")
-            self.creds = Credentials.from_authorized_user_file(self.token_path, self.SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not self.creds or not self.creds.valid:
-            if self.creds and self.creds.expired and self.creds.refresh_token:
-                self.creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    self.credential_path, self.SCOPES)
-                self.creds = flow.run_local_server(port=0)
+        if not self.creds or self.creds.invalid:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                self.credential_path, self.SCOPES
+            )
+            self.creds = flow.run_local_server()
+            # If there are no (valid) credentials available, let the user log in.
             # Save the credentials for the next run
-            with open(self.token_path, 'w') as token:
-                token.write(self.creds.to_json())
+            # with open(self.token_path, "w") as token:
+            #     token.write(self.creds.to_json())
 
-        self.form_service = build('forms', 'directory_v1', credentials=self.creds)
+        print("deberia imprimir esta linea")
+        print(self.creds)
+        self.form_service = discovery.build("forms", "v1", credentials=self.creds)
+        # print("no deberia imprimir esta linea")
 
 
 try:
     # Get data from robot
-    module = GetParams("module")                    #type:ignore # Get command executed 
-    session = GetParams("session")                  #type:ignore # Get Session name
-    result = GetParams("result")                    #type:ignore # Get variable name where save results
+    module = GetParams("module")  # type:ignore # Get command executed
+    session = GetParams("session")  # type:ignore # Get Session name
+    result = GetParams("result")  # type:ignore # Get variable name where save results
+
     if not session:
-        session = SESSION_DEFAULT # Set default session
-    
-    if module == 'connect_forms':
-        credentials_path = GetParams('credentials_path') #type:ignore
-        print(google_directory_path)
-        
-        google_directory = GoogleDirectory(
-            credentials_path,
-            os.path.join(google_directory_path, "token.json")
-        )
+        session = SESSION_DEFAULT  # Set default session
+
+    if module == "connect_forms":
+        credentials_path = GetParams("credentials_path")  # type:ignore
+
+        google_directory = GoogleDirectory(credentials_path)
 
         try:
             google_directory.config_credentials()
             mod_google_directory[session] = google_directory
-            SetVar(result, True)                    #type:ignore
+            SetVar(result, True)  # type:ignore
+
+            # Esto es una prueba para ver si lo crea bien------------------------
+            # Request body for creating a form
+            NEW_FORM = {
+                "info": {
+                    "title": "fomulario nuevo",
+                }
+            }
+
+            NEW_QUESTION = {
+                "requests": [
+                    {
+                        "createItem": {
+                            "item": {
+                                "title": "In what year did the United States land a mission on the moon?",
+                                "questionItem": {
+                                    "question": {
+                                        "required": True,
+                                        "choiceQuestion": {
+                                            "type": "RADIO",
+                                            "options": [
+                                                {"value": "1965"},
+                                                {"value": "1967"},
+                                                {"value": "1969"},
+                                                {"value": "1971"},
+                                            ],
+                                            "shuffle": True,
+                                        },
+                                    }
+                                },
+                            },
+                            "location": {"index": 0},
+                        }
+                    }
+                ]
+            }
+
+            # Creates the initial form
+            result = google_directory.form_service.forms().create(body=NEW_FORM).execute()
+
+            # Adds the question to the form
+            question_setting = (
+                google_directory.form_service.forms()
+                .batchUpdate(formId=result["formId"], body=NEW_QUESTION)
+                .execute()
+            )
+
+            # Prints the result to show the question has been added
+            get_result = google_directory.form_service.forms().get(formId=result["formId"]).execute()
+            print(get_result)
+
+
+            # fin de la prueba ---------------------------------------------------
+
         except Exception as e:
-            SetVar(result, False)                   #type:ignore
-            PrintException()
+            SetVar(result, False)  # type:ignore
+            print(f'imprimio False en la variable "{result}"')
+            PrintException()  # type:ignore
             raise e
 
+        
+    if module == "list_form":
+        print(f"The module {module} is not yet implemented")
+        pass
+
+    if module == "read_form":
+        print(f"The module {module} is not yet implemented")
+        pass
+
+    if module == "create_form":
+        print(f"The module {module} is not yet implemented")
+
+        # Request body for creating a form
+        NEW_FORM = {
+            "info": {
+                "title": "fomulario nuevo",
+            }
+        }
+
+        NEW_QUESTION = {
+            "requests": [
+                {
+                    "createItem": {
+                        "item": {
+                            "title": "In what year did the United States land a mission on the moon?",
+                            "questionItem": {
+                                "question": {
+                                    "required": True,
+                                    "choiceQuestion": {
+                                        "type": "RADIO",
+                                        "options": [
+                                            {"value": "1965"},
+                                            {"value": "1967"},
+                                            {"value": "1969"},
+                                            {"value": "1971"},
+                                        ],
+                                        "shuffle": True,
+                                    },
+                                }
+                            },
+                        },
+                        "location": {"index": 0},
+                    }
+                }
+            ]
+        }
+
+        # Creates the initial form
+        result = google_directory.form_service.forms().create(body=NEW_FORM).execute()
+
+        # Adds the question to the form
+        question_setting = (
+            form_service.forms()
+            .batchUpdate(formId=result["formId"], body=NEW_QUESTION)
+            .execute()
+        )
+
+        # Prints the result to show the question has been added
+        get_result = form_service.forms().get(formId=result["formId"]).execute()
+        print(get_result)
+
+    if module == "delete_form":
+        print(f"The module {module} is not yet implemented")
+        pass
+
+    if module == "export_to_xlsx":
+        print(f"The module {module} is not yet implemented")
+        pass
 
 except Exception as e:
-    exec('PrintException()')
+    exec("PrintException()")
     raise e
--- quickstart.py
+++ quickstart.py
@@ -3,6 +3,7 @@
 # Google libraries
 from googleapiclient import discovery
 from google_auth_oauthlib.flow import InstalledAppFlow
+
 # docs
 # https://github.com/googleapis/google-api-python-client/blob/main/docs/README.md
 
@@ -12,13 +13,13 @@ DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
 
 creds = None
 
-CLIENT_SECRETS_FILE = 'client_secrets.json'
+CLIENT_SECRETS_FILE = "client_secrets.json"
 
 if not creds or creds.invalid:
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
     creds = flow.run_console()
 
-form_service = discovery.build('forms', 'v1', credentials=creds)
+form_service = discovery.build("forms", "v1", credentials=creds)
 
 # Request body for creating a form
 NEW_FORM = {
@@ -29,38 +30,42 @@ NEW_FORM = {
 
 # Request body to add a multiple-choice question
 NEW_QUESTION = {
-    "requests": [{
-        "createItem": {
-            "item": {
-                "title": "In what year did the United States land a mission on the moon?",
-                "questionItem": {
-                    "question": {
-                        "required": True,
-                        "choiceQuestion": {
-                            "type": "RADIO",
-                            "options": [
-                                {"value": "1965"},
-                                {"value": "1967"},
-                                {"value": "1969"},
-                                {"value": "1971"}
-                            ],
-                            "shuffle": True
+    "requests": [
+        {
+            "createItem": {
+                "item": {
+                    "title": "In what year did the United States land a mission on the moon?",
+                    "questionItem": {
+                        "question": {
+                            "required": True,
+                            "choiceQuestion": {
+                                "type": "RADIO",
+                                "options": [
+                                    {"value": "1965"},
+                                    {"value": "1967"},
+                                    {"value": "1969"},
+                                    {"value": "1971"},
+                                ],
+                                "shuffle": True,
+                            },
                         }
-                    }
+                    },
                 },
-            },
-            "location": {
-                "index": 0
+                "location": {"index": 0},
             }
         }
-    }]
+    ]
 }
 
 # Creates the initial form
 result = form_service.forms().create(body=NEW_FORM).execute()
 
 # Adds the question to the form
-question_setting = form_service.forms().batchUpdate(formId=result["formId"], body=NEW_QUESTION).execute()
+question_setting = (
+    form_service.forms()
+    .batchUpdate(formId=result["formId"], body=NEW_QUESTION)
+    .execute()
+)
 
 # Prints the result to show the question has been added
 get_result = form_service.forms().get(formId=result["formId"]).execute()
